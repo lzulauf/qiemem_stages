edc28b281c1997db36b5fb24f032d6e5d3b4e145
Luke Zulauf <lzulauf@gmail.com>
2025-02-25 21:55:04 -0800

    Add Envelope Mode
    This moves all of the envelope mode logic into its own class that wraps
    the envelope manager. This allows the UI to get a pointer to it and
    invoke reinitializing logic when changing modes.
    
    The envelopes now start in the correct state both upon bootup and upon
    entering the independent envelopes mode. That is, it starts with the
    first channel active, all envelopes loaded from save state, and all
    sliders disabled, regardless of position. Sliders will only enable once
    the user has moved them.

---
+++ stages/envelope_mode.h
@@ -0,0 +1,97 @@
+// Copyright 2025 Luke Zulauf.
+//
+// Author: Luke Zulauf (lzulauf@gmail.com)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+// See http://creativecommons.org/licenses/MIT/ for more information.
+
+// -----------------------------------------------------------------------------
+
+// EnvelopeMode
+// Manages modes with DAHDSR (Delay, Attack, Hold, Decay, Sustain, Release)
+// envelopes.
+//
+// The DAHDSR envelope generator is a multi-channel, 6-stage (per channel) envelope
+// generator. Each stage is independently configurable.
+
+#ifndef STAGES_ENVELOPE_MODE_H
+#define STAGES_ENVELOPE_MODE_H
+
+#include "stmlib/stmlib.h"
+
+#include "stages/envelope_manager.h"
+
+namespace stages {
+
+class Settings;
+class Ui;
+
+class EnvelopeMode {
+ public:
+  EnvelopeMode() { }
+  ~EnvelopeMode() { }
+
+  void Init(Settings* settings);
+  void ReInit();
+  void SetUI(Ui* ui);
+
+  void ProcessEnvelopes(IOBuffer::Block* block, size_t size);
+
+ private:
+  EnvelopeManager envelope_manager_;
+  Settings* settings_;
+  Ui* ui_;
+
+  // Keep track of whether latest values have yet to be saved.
+  // -1 means data is not dirty.
+  // a positive time (>=0) means data is dirty and is the number of ticks since it first became dirty.
+  int save_timer_;
+  
+  // The index of the currently selected envelope
+  size_t active_envelope_;
+
+  // bootup delay before processing.
+  int warm_time_;
+
+  // Disallow channel switching for one second on startup (and also every time
+  // the channel is switched - see below).
+  int active_channel_switch_time_;
+
+  // Initial slider positions (set when switching channels). This allows us to
+  // determine once the user has moved a slider sufficiently.
+  // We don't use slider locking, since that feature does not support locking to
+  // an arbitrary value.
+  bool need_to_set_initial_slider_positions_;
+  float initial_slider_positions_[kNumChannels];
+
+  // For each envelope feature, whether we are currently using slider values
+  // Slider positions are ignored for the active envelope until the user moves
+  // them sufficiently to activate them.
+  bool slider_enabled_[kNumChannels];
+
+  void ProcessSixIndependentEgs(IOBuffer::Block* block, size_t size);
+  void ProcessSixIdenticalEgs(IOBuffer::Block* block, size_t size);
+
+  DISALLOW_COPY_AND_ASSIGN(EnvelopeMode);
+};
+
+}  // namespace stages
+
+#endif  // STAGES_ENVELOPE_MODE_H
--- stages/ui.cc
+++ stages/ui.cc
@@ -26,13 +26,14 @@
 //
 // User interface
 
-#include "stages/ui.h"
-#include "stages/chain_state.h"
-
 #include <algorithm>
 
+#include "stages/chain_state.h"
 #include "stages/drivers/leds.h"
+#include "stages/envelope_mode.h"
 #include "stages/settings.h"
+#include "stages/ui.h"
+
 #include "stmlib/system/system_clock.h"
 
 using namespace std;
@@ -47,7 +48,7 @@ const MultiMode Ui::multimodes_[6] = {
   MULTI_MODE_STAGES, // Mode enabled by long pressing the left-most button
   MULTI_MODE_STAGES_ADVANCED,
   MULTI_MODE_SIX_INDEPENDENT_EGS,
-  MULTI_MODE_SIX_EG,
+  MULTI_MODE_SIX_IDENTICAL_EGS,
   MULTI_MODE_OUROBOROS,
   MULTI_MODE_OUROBOROS_ALTERNATE, // Mode enabled by long pressing the right-most button
 };
@@ -60,7 +61,7 @@ const LedColor Ui::palette_[4] = {
   LED_COLOR_OFF,
 };
 
-void Ui::Init(Settings* settings, ChainState* chain_state, CvReader* cv_reader, EnvelopeManager* eg_manager) {
+void Ui::Init(Settings* settings, ChainState* chain_state, CvReader* cv_reader, EnvelopeMode* eg_mode) {
   leds_.Init();
   switches_.Init();
 
@@ -72,7 +73,7 @@ void Ui::Init(Settings* settings, ChainState* chain_state, CvReader* cv_reader, 
   mode_ = UI_MODE_NORMAL;
   chain_state_ = chain_state;
   cv_reader_ = cv_reader;
-  eg_manager_ = eg_manager;
+  eg_mode_ = eg_mode;
 
   if (switches_.pressed_immediate(0)) {
     State* state = settings_->mutable_state();
@@ -323,7 +324,7 @@ void Ui::MultiModeToggle(const uint8_t i) {
     state->multimode = (uint8_t) multimodes_[i];
     settings_->SaveState();
     chain_state_->start_reinit();
-    eg_manager_->ReInit();
+    eg_mode_->ReInit();
   }
 }
 
@@ -466,7 +467,8 @@ void Ui::UpdateLEDs() {
             slider_led_counter_[i] ? LED_COLOR_GREEN : LED_COLOR_OFF);
       }
 
-    } else if (multimode == MULTI_MODE_SIX_EG || multimode == MULTI_MODE_SIX_INDEPENDENT_EGS) {
+    } else if (multimode == MULTI_MODE_SIX_IDENTICAL_EGS ||
+               multimode == MULTI_MODE_SIX_INDEPENDENT_EGS) {
       // LEDs update for 6EG and 6IEG modes
       for (size_t i = 0; i < kNumChannels; ++i) {
         leds_.set(LED_GROUP_UI + i, led_color_[i]);
